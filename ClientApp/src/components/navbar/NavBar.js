1690d912fbe2454d8e9154abe20a933043e531ba
h0pped <Breakingsnoopy@gmail.com>
2020-01-01 13:02:33 +0200

    navbar cart button and auto-updated count of products in cart(redux)

--- ClientApp/src/components/navbar/NavBar.js
+++ ClientApp/src/components/navbar/NavBar.js
@@ -1,26 +1,56 @@
 import React, { Component } from 'react';
 import './NavBar.css';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { connect } from "react-redux";
 
-export default class NavBar extends Component{
-    render(){
-        return(
+import get from 'lodash.get';
+
+class NavBarius extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
             <div>
-            
-         <div className="columns">
-             <div className="column is-full has-text-centered">
-             <nav className="breadcrumb is-large is-centered" aria-label="breadcrumbs">
-                 <ul>
-                   <li><Link to="/Комоды">Комоды</Link></li>
-                   <li><Link to="/МягкаяЧасть">Мягкая часть</Link></li>
-                   <li><Link to="/СтолыИСтулья">Столы и Стулья</Link></li>
-                   <li><Link to="/Часы">Часы</Link></li>
-                   <li><Link to="/Разное">Разное</Link></li>
+
+                <div className="columns">
+                    <div className="column is-full has-text-centered">
+                        <nav className="breadcrumb is-large is-centered" aria-label="breadcrumbs">
+                            <ul>
+                                <li><Link to="/Комоды">Комоды</Link></li>
+                                <li><Link to="/МягкаяЧасть">Мягкая часть</Link></li>
+                                <li ><Link to="/СтолыИСтулья">Столы и Стулья</Link></li>
+                                <li><Link to="/Часы">Часы</Link></li>
+                                <li className="is-right"><Link to="/Разное">Разное</Link></li>
+                                <li ><Link className="cartButton" to="/Корзина"><span class="icon">
+                                    <i class="fas fa-shopping-cart"></i>
+                                </span>{this.props.cart.count}</Link></li>
                  </ul>
                 </nav>
-             </div>
-         </div>   
+                </div>
+            </div>   
+            </div >
+        )
+    }
+}
+class NavBar extends Component {
+
+
+    render() {
+        return (
+            <div className="header App text-center">
+                <NavBarius cart={this.props.cart} />
             </div>
-        )    
+        )
+    }
 }
+
+const mapState = (state) => {
+    return {
+        cart: {
+            count: get(state, 'cart.products').length
+        }
+    }
 }
+
+export default connect(mapState)(NavBar);
