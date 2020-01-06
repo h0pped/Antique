61f47e8e01b9ce1371441dda410fc0040b980446
h0pped <Breakingsnoopy@gmail.com>
2020-01-06 13:32:52 +0200

    cart total price of products, form order button, responsive for mobiles

--- ClientApp/src/components/Cart/Cart.js
+++ ClientApp/src/components/Cart/Cart.js
@@ -3,6 +3,7 @@ import get from 'lodash.get';
 import PropTypes from "prop-types";
 import * as cartActions from '../productservice/reducer';
 import { connect } from "react-redux";
+import { Link } from 'react-router-dom'
 
 const propTypes = {
     cart: PropTypes.object.isRequired,
@@ -19,6 +20,15 @@ class Cart extends Component {
     }
 
     render() {
+        if(this.props.cart.products.length===0){
+            return(
+                <div className="column is-full has-text-centered">
+                    <h1 className="is-size-3 has-text-black">Ваша корзина пустая</h1>
+                </div>
+            )
+        }
+        else{
+
         return (
             <div>
                 <div className="columns">
@@ -50,9 +60,23 @@ class Cart extends Component {
                             </tbody>
                         </table>
                     </div>
+                    <div class="columns">
+                    <div className="column is-offset-2  has-text-left is-one-third-dekstop is-two-tablet is-one-third-fullhd  is-full-mobile">
+                                <h3 className="is-size-4">Общая стоимость: {this.props.cart.total.toFixed(2)}грн.</h3>
+                    </div>
+                    <div className="column">
+                    <Link to="/order"><button className="button is-dark"> <span class="icon">
+                                    <i class="fa fa-shopping-cart"></i>
+                                </span> <p> Оформить заказ</p></button></Link>
+                    </div>
+                    <div className="column is-3 is-zero-tablet   is-zero-mobile">
+                    </div>
+                    </div>
                 </div>
 
         )
+    }
+
     }
 }
 
