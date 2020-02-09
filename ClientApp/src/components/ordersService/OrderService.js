7edc4b14c1a54d432f762cbaa341a8902e0af8d8
h0pped <Breakingsnoopy@gmail.com>
2020-02-09 20:44:14 +0200

    orders table is ready (pagination works)

--- ClientApp/src/components/ordersService/OrderService.js
+++ ClientApp/src/components/ordersService/OrderService.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Pagination from '../Pagination/Pagination'
 
 import PropTypes from "prop-types";
@@ -6,58 +6,60 @@ import { connect } from "react-redux";
 import OrdersTable from './OrdersTable'
 import get from 'lodash.get';
 
- class OrderService extends Component{
-    constructor(){
+class OrderService extends Component {
+    constructor() {
         super();
         this.state = {
-            apiUrl: "/api/Orders", 
-            error:null,
+            apiUrl: "/api/Orders",
+            error: null,
             isloaded: false,
-            orders:[],
-            currentPage:1,
-            OrdersPerPage:2,   
+            orders: [],
+            currentPage: 1,
+            OrdersPerPage: 12,
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         console.log(this.props.category);
         let url = this.state.apiUrl;
-        
-            if(this.props.category){
-                console.log(this.props.category);
-                 url = this.state.apiUrl;
 
-            }
-            console.log(url);
+        if (this.props.category) {
+            console.log(this.props.category);
+            url = this.state.apiUrl;
+
+        }
+        console.log(url);
 
         fetch(url)
-        .then( res => res.json() )
-        .then( json => { this.setState( { orders : json, isloaded:true });
-        },(error)=>{
-            this.setState({isloaded:true, error});
-        });
+            .then(res => res.json())
+            .then(json => {
+                this.setState({ orders: json, isloaded: true });
+            }, (error) => {
+                this.setState({ isloaded: true, error });
+            });
     }
-    render(){
-        const {orders,isloaded,error,currentPage,OrdersPerPage} = this.state;
-        const indexOfLastProduct= currentPage*OrdersPerPage;
-        const indexOfFirstProduct= indexOfLastProduct-OrdersPerPage;
-        const currentOrders= orders.slice(indexOfFirstProduct,indexOfLastProduct);
+    render() {
+        const { orders, isloaded, error, currentPage, OrdersPerPage } = this.state;
+        const indexOfLastProduct = currentPage * OrdersPerPage;
+        const indexOfFirstProduct = indexOfLastProduct - OrdersPerPage;
+        const currentOrders = orders.slice(indexOfFirstProduct, indexOfLastProduct);
         console.log(orders);
-        const paginate = (pageNumber) =>{
-            if(pageNumber<1||pageNumber>Math.ceil(orders.length/OrdersPerPage))
-            {
+        const paginate = (pageNumber) => {
+            if (pageNumber < 1 || pageNumber > Math.ceil(orders.length / OrdersPerPage)) {
                 return;
             }
             else
-            this.setState({currentPage:pageNumber});
+                this.setState({ currentPage: pageNumber });
             window.scrollTo(0, 0);
-        } 
-    return(
-        <div>
-             <OrdersTable orders={currentOrders} isloaded={isloaded} error={error}/>
-            <Pagination productsPerPage={OrdersPerPage} totalProducts={orders.length} paginate={paginate} currentPage={currentPage}/>
-        </div>
+        }
+        return (
+            <div>
+               
+                    <OrdersTable orders={currentOrders} isloaded={isloaded} error={error} />
+                    <Pagination productsPerPage={OrdersPerPage} totalProducts={orders.length} paginate={paginate} currentPage={currentPage} />
+                </div>
+
         )
-}
+    }
 }
 export default OrderService;
--- ClientApp/src/components/ordersService/OrdersTable.js
+++ ClientApp/src/components/ordersService/OrdersTable.js
@@ -1,26 +1,60 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import ProductDescription from '../productdescription/ProductDescription';
 
-class OrdersTable extends Component{
-    render(){
-        const {error, isloaded, orders} = this.props;
-        if(error){
-            return(
+class OrdersTable extends Component {
+    render() {
+        const { error, isloaded, orders } = this.props;
+        if (error) {
+            return (
                 <div>Ошибка при загрузке товаров: {error.message}</div>
             )
         } else if (!isloaded) {
             return <div>Загрузка...</div>;
         }
-        else{
-            return(<div>
-                {orders.map(order => (
-                    <div>
+        else {
+            if (orders) {
 
-                    <h3>{order.name}</h3>
-                    <br></br>
+
+                return (<div>
+                    <div className='column is-full has-text-centered'>
+                        <h2 className="is-size-3 new-arrivals-header has-text-dark">Последние заказы</h2>
+                        <div className='column is-full has-text-centered'>
+                            <table className="table is-fullwidth is-bordered is-hoverable">
+                                <thead>
+                                    <tr>
+                                        <th>Артикул</th>
+                                        <th>Имя</th>
+                                        <th>Город</th>
+                                        <th>Почта</th>
+                                        <th>Сумма Заказа</th>
+                                        <th>Подробнее</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {orders.map((order, index) => (
+                                        <tr key={index}>
+                                            <td>{order.id}</td>
+                                            <td>{order.name + " " + order.surname}</td>
+                                            <td>{order.city}</td>
+                                            <td>{order.delivery + "\t№ " + order.deliveryNum}</td>
+                                            <td>{order.totalPrice.toFixed(2)} грн.</td>
+                                            <td><button className="button is-black is-outlined is-rounded" ><span class="icon is-small">
+                                                <i class="fas fa-bars"></i>
+                                            </span></button></td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
-                    ))}
-            </div>)
+                </div>)
+            }
+            else {
+                return (<div className='column is-full has-text-centered'>
+                    <h2 className="is-size-3 new-arrivals-header has-text-dark">Заказы отсутствуют</h2>
+                </div>)
+            }
         }
-}}
+    }
+}
 export default OrdersTable;
